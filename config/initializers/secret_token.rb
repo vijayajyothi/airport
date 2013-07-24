dcc19187e00f6bcbbe55bbc9cc61f277ba1a2f45
vijayajyothi <jyothis.kalangi@gmail.com>
2013-07-24 10:47:45 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirportSamp::Application.config.secret_token = '5fcd119b243cff977a8f82dd6acefab0fb39d5d8ed3f7fd3177ac9677f30ee5c6ca0aeaadff94bedc2de9a272933abf4e431f8a94e3c6056f1acc042c75fef38'
